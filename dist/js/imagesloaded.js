6e03de717484d22a59fad98fe5631deddeeab9d7
Vicka <vicrogatinskaya@gmail.com>
2022-06-02 21:18:52 +0200

    removed extra fonts

--- dist/js/imagesloaded.js
+++ dist/js/imagesloaded.js
@@ -1,10 +1,3 @@
-/*! This file is auto-generated */
-/*!
- * imagesLoaded PACKAGED v4.1.4
- * JavaScript is all like "You images are done yet or what?"
- * MIT License
- */
-
 !(function (e, t) {
   'function' == typeof define && define.amd
     ? define('ev-emitter/ev-emitter', t)
